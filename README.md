39999de9d98d506ea9e546fd193ae6c90e84187d
Camilo Reyes <reyesc@gmail.com>
2016-01-23 08:34:10 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+﻿#Proper Error Handling In JavaScript
---
+++ app.js
@@ -0,0 +1,8 @@
+﻿var http = require('http');
+
+var app = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+});
+
+module.exports = app;
