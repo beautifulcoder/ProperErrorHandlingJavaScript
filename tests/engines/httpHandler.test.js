9991a8eb037d3033daa6689962592bc683c797a9
Camilo Reyes <reyesc@gmail.com>
2016-01-28 20:07:16 -0600

    Refactor mocks in unit tests

--- tests/engines/httpHandler.test.js
+++ tests/engines/httpHandler.test.js
@@ -1,51 +1,25 @@
 ﻿var target = require('../../engines/httpHandler');
+var response = require('./response.mock');
 var should = require('should');
 
 describe('An HTTP engine', function () {
     it('handles errors', function () {
-        var res = responseMock();
+        var res = response();
         target({ message: 'error' }, res);
         res.getResult().should.equal('500text/plainError: error');
     });
 
     it('handles responses', function () {
-        var res = responseMock();
+        var res = response();
         var content = { type: 'type', response: 'message' };
         target(null, res, content);
         res.getResult().should.equal('200typemessage');
     });
 
     it('handles responses with no-cache', function () {
-        var res = responseMock();
+        var res = response();
         var content = { type: 'type', response: 'message', nocache: true };
         target(null, res, content);
         res.getResult().should.equal('200typeno-cachemessage');
     });
-
-    function responseMock() {
-        var result = '';
-
-        return {
-            writeHead: writeHead,
-            end: end,
-            getResult: getResult
-        };
-
-        function writeHead(returnCode, headers) {
-            result += returnCode;
-            for (var prop in headers) {
-                if (headers.hasOwnProperty(prop)) {
-                    result += headers[prop];
-                }
-            }
-        }
-
-        function end(body) {
-            result += body;
-        }
-
-        function getResult() {
-            return result;
-        }
-    }
 });
---
+++ tests/engines/response.mock.js
@@ -0,0 +1,28 @@
+﻿function response() {
+    var result = '';
+
+    return {
+        writeHead: writeHead,
+        end: end,
+        getResult: getResult
+    };
+
+    function writeHead(returnCode, headers) {
+        result += returnCode;
+        for (var prop in headers) {
+            if (headers.hasOwnProperty(prop)) {
+                result += headers[prop];
+            }
+        }
+    }
+
+    function end(body) {
+        result += body;
+    }
+
+    function getResult() {
+        return result;
+    }
+}
+
+module.exports = response;
