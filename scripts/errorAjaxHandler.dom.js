63d6edac3b147a2d7264d41b82762997f198307d
Camilo Reyes <reyesc@gmail.com>
2016-01-31 07:19:40 -0600

    Add error message to Ajax handler

--- scripts/errorAjaxHandler.dom.js
+++ scripts/errorAjaxHandler.dom.js
@@ -1,8 +1,10 @@
 ﻿window.addEventListener('error', function (err) {
     var stack = err.error.stack;
+    var message = err.error.toString();
     if (stack) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/log', true);
-        xhr.send(stack);
+        message += '\n' + stack;
     }
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/log', true);
+    xhr.send(message);
 });
